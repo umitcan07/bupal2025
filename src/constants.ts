f19640ca1a1df432ffdf2b7fcccd37f22c9195ed
Ümit Can Evleksiz <evleksizumitcan@gmail.com>
2025-09-30 23:00:19 +0300

    smol fikses

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+export const EXTERNAL_URLS = {
+  WHATSAPP: "https://chat.whatsapp.com/EXbnMjTWNiu5uyoZyM3yN4",
+  FORM: "https://cryptpad.fr/form/#/2/form/view/WUMFnKJV2M1ncO2mdHqxZKd0BMmfawv57ygxnV-XOC4/",
+  INSTAGRAM: "https://instagram.com/bupalestine",
+  TWITTER: "https://twitter.com/bupalestine",
+} as const;
